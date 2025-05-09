5cbcd0f3a6631fd78a0694a4cf614bf3f2f2a53d
Everton Menezes <everton.antonio@outlook.com>
2025-05-09 19:16:33 -0300

    Adicionar cálculo da área de um triângulo e reformular menus no programa principal

--- Aula01/Exercicios.py
+++ Aula01/Exercicios.py
@@ -1,6 +1,28 @@
-base = float(input('Digite a sua base para continuar...'))
-altura = float(input('Digite a sua altura para continuar...'))
-area = (base * altura)/2
-print(area)
+#base = float(input('Digite a sua base para continuar...'))
+#altura = float(input('Digite a sua altura para continuar...'))
+#area = (base * altura)/2
+#print(area)
+
+# Aula - 02 - Exercícios
+#numeros =[1,2,3,4,5,6,7,8,9,10]
+#numeros.reverse()
+#for r in numeros:
+#  print("Digite o Número",r,r)
+
+numeros = []
+for i in range(11):
+    n = input(f'Digite o {i + 1}º valor: ')
+    numeros.append(n)
+
+#for j in range(9, -1, -1):
+ #   print(f't: {j}')
 
 
+ # Aula - 03 - Exercícios 
+numero = int(input('Digite um número: '))
+
+while numero < 1:
+    int(input('Digite um número positivo'))
+for i in range(1, 11):
+    resultado = numero * i
+    print(f'{numero} x {i} = {resultado}')
--- Aula01/principal.py
+++ Aula01/principal.py
@@ -43,28 +43,97 @@ menu_livro = """
 4 - Ver livro por Id
 0 - Voltar ao menu anterior
 """
-
+# Inicializando a tabela de autores
+tabela_autores = []
+# Inicializando a tabela de editoras
+tabela_editoras = []
+# Inicializando a tabela de categorias
+tabela_categorias = []
+# Inicializando a tabela de livros
+tabela_livros = []
 while True:
     print(menu_principal)
     op = input('Digite a opção: ')
 
     if op == '0':
         break
-    if op == '1':
-        print('Opção escolhida: Categorias')
-        input('Digite <ENTER> para continuar...')
-    elif op == '2':
-        print('Opção escolhida: Editoras')
-        input('Digite <ENTER> para continuar...')
+    elif op == '1':
+        print(menu_categoria)
+        input('\nDigite <ENTER> para continuar...\n')
+    elif op == '2': # else if
+        print(menu_editora)
+        input('\nDigite <ENTER> para continuar...\n')
     elif op == '3':
-        print('Opção escolhida: Autores')
-        input('Digite <ENTER> para continuar...')
+        while True:
+            print(menu_autor)
+            opcao_autor = input('Digite a opção: ')
+            if opcao_autor == '0':
+                break
+            elif opcao_autor == '1':
+                if tabela_autores == []:
+                    print('Nenhum autor cadastrado!')
+                    input('\nDigite <ENTER> para continuar...\n')
+                print('ID |Nome | E-mail | Telefone | Biografia')
+                for index, autor in enumerate(tabela_autores):
+                    print(f'{index + 1} | {autor[0]} | {autor[1]} | {autor[2]} | {autor[3]}')
+            elif opcao_autor == '2':
+                nome_autor = input('Digite o nome do autor: ')
+                telefone_autor = input('Digite o telefone do autor: ')
+                bio_autor = input('Digite a biografia do autor: ')
+                email_autor = input('Digite o e-mail do autor: ')
+                novo_autor = {
+                    'nome': nome_autor,
+                    'email': email_autor,
+                    'telefone': telefone_autor,
+                    'biografia': bio_autor
+                }
+            elif opcao_autor == '3':
+                if tabela_autores == []:
+                    print('Nenhum autor cadastrado!')
+                    input('\nDigite <ENTER> para continuar...\n')
+                else:
+                    id_autor = int(input('Digite o ID do autor a ser excluído: '))
+                    tabela_autores.pop(id_autor - 1)
+                    print(f'Autor com ID {id_autor} excluído com sucesso!')
+                    input('\nDigite <ENTER> para continuar...\n')
+            elif opcao_autor == '4':
+                if tabela_autores == []:
+                    print('Nenhum autor cadastrado!')
+                    input('\nDigite <ENTER> para continuar...\n')
+                else:
+                    id_autor = int(input('Digite o ID do autor a ser exibido: '))
+                    if id_autor > len(tabela_autores):
+                        print('ID inválido!')
+                    else:
+                        print('ID |Nome | E-mail | Telefone | Biografia')
+                        print(f'{tabela_autores[id_autor][0]} | {tabela_autores[id_autor - 1][1]} | {tabela_autores[id_autor - 1][2]} | {tabela_autores[id_autor - 1][3]}')
+                        input('\nDigite <ENTER> para continuar...\n')
+            elif opcao_autor == '5':
+                if tabela_autores == []:
+                    print('Nenhum autor cadastrado!')
+                    input('\nDigite <ENTER> para continuar...\n')
+                else:
+                    id_autor = int(input('Digite o ID do autor a ser editado: '))
+                    if id_autor > len(tabela_autores):
+                        print('ID inválido!')
+                    else:
+                        nome_autor = input('Digite o novo nome do autor: ')
+                        telefone_autor = input('Digite o novo telefone do autor: ')
+                        bio_autor = input('Digite a nova biografia do autor: ')
+                        email_autor = input('Digite o novo e-mail do autor: ')
+                        tabela_autores[id_autor - 1][0] = nome_autor
+                        tabela_autores[id_autor - 1][1] = email_autor
+                        tabela_autores[id_autor - 1][2] = telefone_autor
+                        tabela_autores[id_autor - 1][3] = bio_autor
+                        print(f'Autor com ID {id_autor} editado com sucesso!')
+                        input('\nDigite <ENTER> para continuar...\n')
+            else:
+                print('Opção inválida!')
     elif op == '4':
-        print('Opção escolhida: Livros')
-        input('Digite <ENTER> para continuar...')
+        print(menu_livro)
+        input('\nDigite <ENTER> para continuar...\n')
     else:
         print('Opção inválida!')
 
-
-print('Programa encerrado!')    
+print('Programa encerrado!')  
 
